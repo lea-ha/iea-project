9a6e3a68e6e333a251ee218c05a62bde5ebc6d59
lea-ha <lea.haidamous@lau.edu>
2025-04-03 17:53:30 +0300

    add bfs pathfinding method

--- backend/src/main/java/astar/Astar.java
+++ backend/src/main/java/astar/Astar.java
@@ -55,6 +55,52 @@ public class Astar {
         return null; // No path found
     }
 
+    public static List<Coordinate> bfs(
+            int[][] grid, Agent agent, Map<SubNode, Integer> reservations, int maxPathLength) {
+        Coordinate start = agent.start();
+        Coordinate goal = agent.goal();
+
+        Queue<Node> queue = new LinkedList<>();
+
+        Node startNode = new Node(Coordinate.with(start.x(), start.y()), 0, new ArrayList<>());
+        startNode.path.add(startNode); // Add the starting node to its own path.
+        queue.add(startNode);
+
+        Set<SubNode> visited = new HashSet<>();
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            // EARLY PRUNING
+            if (current.g > maxPathLength) {
+                continue;
+            }
+
+            if (visited.contains(SubNode.of(current.coordinate, current.g))) {
+                continue;
+            }
+            visited.add(SubNode.of(current.coordinate, current.g));
+
+            if (current.coordinate.equals(goal) && current.g == maxPathLength) {
+                return getPath(current);
+            }
+
+            List<Coordinate> neighbors = getNeighbors(current.coordinate, grid);
+            for (Coordinate neighbor : neighbors) {
+                int t = current.g + 1;
+                SubNode neighborNode = SubNode.of(neighbor, t);
+                if (reservations.containsKey(neighborNode) && !reservations.get(neighborNode).equals(agent.id())) {
+                    continue; //reserved by another agent
+                }
+
+                List<Node> newPath = new ArrayList<>(current.path);
+                newPath.add(new Node(neighbor, t, newPath));
+                queue.add(new Node(neighbor, t, newPath));
+            }
+        }
+        return null; // No path found
+    }
+
     private static List<Coordinate> getNeighbors(Coordinate currentCoordinate, int[][] grid) {
         int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {0, 0}};
         List<Coordinate> neighbors = new ArrayList<>();
